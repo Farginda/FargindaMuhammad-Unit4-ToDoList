4396086f4d50dbe8aaff5032a78d851fde056e2d
Farginda <Farginda@wcw-stud-145-109-60-62.wireless.uva.nl>
2018-11-27 14:36:35 +0100

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Farginda on 11/27/18.
+//  Copyright © 2018 Farginda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
